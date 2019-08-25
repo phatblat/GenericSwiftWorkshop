bc48851e1a3557434362b48d270679bcd02ed0f1
Ben Chatelain <ben@octop.ad>
2019-08-25 11:29:09 -0600

    Identifier and Identified

--- GenericSwiftWorkshop/Model.swift
+++ GenericSwiftWorkshop/Model.swift
@@ -8,24 +8,49 @@
 
 import Foundation
 
-protocol IDType: Codable, Hashable {
-    associatedtype Value
-    var value: Value { get }
-    init(value: Value)
+//protocol IDType: Codable, Hashable {
+//    associatedtype Value
+//    var value: Value { get }
+//    init(value: Value)
+//}
+//
+//extension IDType {
+//    init(_ value: Value) { self.init(value: value) }
+//}
+
+struct Identifier<Model, Value>
+    where Value: Codable & Hashable
+{
+    let value: Value
+    init(_ value: Value) { self.value = value }
 }
 
-extension IDType {
-    init(_ value: Value) { self.init(value: value) }
+extension Identifier: Codable, Hashable {
+    init(from decoder: Decoder) throws {
+        self.init(try decoder.singleValueContainer().decode(Value.self))
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(value)
+    }
 }
 
-struct User: Codable, Hashable {
-    struct ID: IDType { let value: Int }
+// Identifyable in stdlib?
+protocol Identified: Codable {
+    associatedtype IDType: Codable & Hashable
+    typealias ID = Identifier<Self, IDType>
+    var id: ID { get }
+}
+
+struct User: Identified, Hashable {
+    typealias ID = Identifier<User, Int>
     let id: ID
     let name: String
 }
 
 struct Document: Codable, Hashable {
-    struct ID: IDType { let value: String }
+    typealias ID = Identifier<Document, String>
     let id: ID
     let title: String
 }
