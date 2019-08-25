7c42e9f410e43edc7f34e0f0b0757aa963eccc49
Ben Chatelain <ben@octop.ad>
2019-08-25 08:57:21 -0600

    Initial Commit

---
+++ GenericSwiftWorkshop/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GenericSwiftWorkshop
+//
+//  Created by Ben Chatelain on 8/25/19.
+//  Copyright © 2019 Ben Chatelain. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
