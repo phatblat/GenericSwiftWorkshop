8582e15b26443aa82074ade622db299f3d741723
Ben Chatelain <ben@octop.ad>
2019-08-25 11:51:05 -0600

    type erasers

---
+++ GenericSwiftWorkshop/TypeErasers.swift
@@ -0,0 +1,43 @@
+//
+//  TypeErasers.swift
+//  GenericSwiftWorkshop
+//
+//  Created by Ben Chatelain on 8/25/19.
+//  Copyright © 2019 Ben Chatelain. All rights reserved.
+//
+
+import Foundation
+
+protocol Processor {
+    associatedtype Input
+    associatedtype Output
+    func process(from input: Input) -> Output
+}
+
+struct AnyProcessor<Input, Output> {
+    let base: Any
+
+    private let _process: (Input) -> Output
+
+    init<P: Processor>(_ processor: P)
+    where
+        P.Input == Input,
+        P.Output == Output
+    {
+        _process = processor.process
+        base = processor
+    }
+
+    func process(from input: Input) -> Output {
+        return _process(input)
+    }
+}
+
+struct SomeProcessor: Processor {
+    typealias Input = Int
+    typealias Output = String
+    func process(from input: Int) -> String { return "" }
+}
+
+let p = AnyProcessor(SomeProcessor())
+let s = p.base as? SomeProcessor
